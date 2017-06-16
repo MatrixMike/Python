f4939e73e9cd8e3ea017769021eda2782c0a605d
MatrixMike <matroxmike@icloud.com>
2017-06-16 21:34:46 +1000

    extension 1

--- python3.py
+++ python3.py
@@ -2,34 +2,38 @@
 # mod 16.06.2017 13:15:54
 import turtle
 
-turtle.left(20)
+def turt():
+    turtle.left(20)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
+    turtle.left(90)
+    turtle.forward(100)
 
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
+for i in range(1, 30, 1):
+	turt()
+	
+	
+#turtle.left(20)
 
-turtle.left(20)
+#turtle.forward(100)
+#turtle.left(90)
+#turtle.forward(100)
+#turtle.left(90)
+#turtle.forward(100)
+#turtle.left(90)
+#turtle.forward(100)
 
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
+#turtle.left(20)
 
-turtle.left(20)
+#turtle.forward(100)
+#turtle.left(90)
+#turtle.forward(100)
+#turtle.left(90)
+#turtle.forward(100)
+#turtle.left(90)
+#turtle.forward(100)
 
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-
-turtle.exitonclick()
+#turtle.exitonclick()
