81b6a39cd8a5949b5141321ae34fa3e69b59ff40
MatrixMike <matroxmike@icloud.com>
2017-06-16 15:33:29 +1000

    pep8

--- DataIntro/eg3.py
+++ DataIntro/eg3.py
@@ -1,13 +1,15 @@
 #eg3   on python3
 
-shopping_list =["milk", "bread", "three"]
+shopping_list = ["milk", "bread", "three"]
+
 
 def show_list():
     print()
     print("list looks like this now=>")
     for item in shopping_list:
         print(item)
-        
+
+
 def check_list():
     if "eggs" not in shopping_list:
         print("Well we can't have that!")
@@ -23,8 +25,7 @@ if "milk" in shopping_list:
 show_list()
 
 
-        
-check_list()    
+check_list()
 print ("second try")
 check_list()
 # now to remove the bread
@@ -32,6 +33,3 @@ shopping_list.remove("bread")
 show_list()
 #shopping_list.remove("water")
 show_list()
-
-
-
