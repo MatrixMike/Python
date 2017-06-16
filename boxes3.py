fce56904f75c55ee0f81fe989fbf9370781dfb8e
MatrixMike <matroxmike@icloud.com>
2017-06-16 13:30:56 +1000

    renaming some

---
+++ boxes3.py
@@ -0,0 +1,35 @@
+# print ("hi there")
+# mod 16.06.2017 13:15:54
+import turtle
+
+turtle.left(20)
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.left(20)
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.left(20)
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.exitonclick()
