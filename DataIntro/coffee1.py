a2db9806e3f615f8f17253ad7b4100cee0b1a42d
Mike Hewitt <matroxmike@icloud.com>
2017-06-16 14:53:17 +1000

    alternate file (db)

--- DataIntro/coffee1.py
+++ DataIntro/coffee1.py
@@ -1,15 +1,16 @@
 #  coffee csv file
 
 import csv
-f=open("coffee.csv")
+#/home/mikeh/eca/ECA/ECA running costs.csv
+f = open("/home/mikeh/eca/ECA/ECA running costs.csv")
+#f = open("coffee.csv")
 for row in csv.reader(f):
     print(row)
 
 
 print("separator\n=======")
-f=open("coffee.csv")
+#f = open("coffee.csv")
+f = open("/home/mikeh/eca/ECA/ECA running costs.csv")
 #f=open("D:\FromLexarUSB\ClassLists\FormList-20120528-142931.csv")
 for row in csv.reader(f):
-    print(row[2])
-
-    
+    print(row[4])
