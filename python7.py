e43926228799ea917ec467ccb971efca321e568b
MatrixMike <matroxmike@icloud.com>
2017-06-16 22:22:16 +1000

    extension 2

--- python7.py
+++ python7.py
@@ -1,16 +1,18 @@
 import turtle
 
+angle1 = 90
+distance1 = 50
 
 def tilted_square():
     turtle.left(angle)
-    turtle.forward(50)
-    turtle.left(90)
-    turtle.forward(50)
-    turtle.left(90)
-    turtle.forward(50)
-    turtle.left(90)
-    turtle.forward(50)
-    turtle.left(90)
+    turtle.forward(distance1)
+    turtle.left(angle1)
+    turtle.forward(distance1)
+    turtle.left(angle1)
+    turtle.forward(distance1)
+    turtle.left(angle1)
+    turtle.forward(distance1)
+    turtle.left(angle1)
 
 angle = 20            # <--
 for i in range(1, 12, 1):
