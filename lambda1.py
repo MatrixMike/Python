4df4fbb421e0b136a81c1c12b2ca8ce464c3dd56
MatrixMike <matroxmike@icloud.com>
2017-06-17 18:41:03 +1000

    some functional tests 3

--- lambda1.py
+++ lambda1.py
@@ -9,5 +9,7 @@
 
 do_it = lambda f, *args: f(*args)
 hello = lambda first, last: print("Hello", first, last)
+do_all_funcs = lambda fns, *args: [list(map(fn, *args)) for fn in fns]
 bye = lambda first, last: print("Bye", first, last)
 _ = list(map(do_it, [hello, bye],  ['David', 'Jane'], ['Mertz', 'Doe']))
+_ = do_all_funcs([hello, bye], ['David', 'Jane'], ['Mertz', 'Doe'])
