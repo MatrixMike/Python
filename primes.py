1f7e9cdadff897de00a127f06a1924d657c0e4aa
MatrixMike <matroxmike@icloud.com>
2017-06-17 19:04:27 +1000

    some functional tests 6

---
+++ primes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  primes.py
+#
+#  Copyright 2017 Michael Hewitt <mikeh@electroteach.com>
+#
+
+
+def get_primes():
+
+    "Simple lazy Sieve of Eratosthenes"
+    candidate = 2
+    found = []
+    while True:
+        if all(candidate % prime != 0 for prime in found):
+            yield candidate
+            found.append(candidate)
+        candidate += 1
+
+primes = get_primes()
+next(primes), next(primes), next(primes)
+print(next(primes))
+
+for _, prime in zip(range(10), primes):
+    print(prime, end=" ")
+for _, prime in zip(range(10), primes):
+    print(prime, end=" ")
